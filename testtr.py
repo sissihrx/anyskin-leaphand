0dc2fce2330b099c415df077248e5d1abbe70e46
sissihrx <sissi.hu@hotmail.com>
2025-04-18 16:40:54 -0400

    .

--- testtr.py
+++ testtr.py
@@ -22,15 +22,19 @@ p.setCollisionFilterPair(robot_id, robot_id, 8, 10, enableCollision=0)
 
 positions = np.loadtxt("positionsnew.txt")
 data = []
+data.append(positions[0])
 last = positions[0]
-for i in range(1, 200):
-    for k in range(1, 51):
+for i in range(16): p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=i, controlMode=p.POSITION_CONTROL, targetPosition=positions[0][i], force=100)
+
+for i in range(1, len(positions)):
+    if i % 50 == 0: np.savetxt("positionstr.txt", np.array(data))
+    for k in range(1, 21):
         curr = []
         for j in range(16):
             joint_info = p.getJointInfo(robot_id, j)
             a = positions[i][j] * (joint_info[9] - joint_info[8]) + joint_info[8]
-            prev = positions[i-1][j] * (joint_info[9] - joint_info[8]) + joint_info[8]
-            a = prev + (a - prev) * k / 50
+            prev = last[j] * (joint_info[9] - joint_info[8]) + joint_info[8]
+            a = prev + (a - prev) * k / 20
             curr.append(a)
             p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=j, controlMode=p.POSITION_CONTROL, targetPosition=a, force=100)
         for _ in range(240): 
@@ -45,13 +49,15 @@ for i in range(1, 200):
         #     break
         if self_collisions:            
             for x in range(16): p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=j, controlMode=p.POSITION_CONTROL, targetPosition=last[x], force=100)
+            print(i)
             break
-        elif k == 50:
+        elif k == 20:
             data.append(curr)
-            last = curr.copy()
+            last = curr.copy() #last is bad
             break
 
 
 p.disconnect()
-np.savetxt("positionstr.txt", data)
+np.savetxt("positionstr.txt", np.array(data))
 
+#LAST IS BAD
